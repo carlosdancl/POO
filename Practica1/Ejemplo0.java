77d8b6a72b9b51c3eddfc2cdbd4b75e0362fbda1
carlosdancl <contreras.l.carlos.daniel@gmail.com>
2025-08-18 19:10:06 -0600

    Primer ejemplo Java

---
+++ Practica1/Ejemplo0.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ejemplo0 {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+        System.out.print("Ingresa el valor del operador 1: ");
+        int op1 = e.nextInt();
+        System.out.println();
+        System.out.println("Ingresa el valor del operador 2: ");
+        int op2 = e.nextInt();
+        e.close();
+        op1 += op2;
+        boolean res = op1 > 0 ? true : false;
+        System.out.print("el resultado de la comparación es "+res);
+    }
+}
