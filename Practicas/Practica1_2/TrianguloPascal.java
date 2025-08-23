bed3a754cb3c3a8595209e26635e050be43d6322
amglezb <amglez.edu@gmail.com>
2025-08-22 21:38:05 -0600

    pascal_v2

--- Practicas/Practica1_2/TrianguloPascal.java
+++ Practicas/Practica1_2/TrianguloPascal.java
@@ -1,24 +1,41 @@
 import java.util.Scanner;
-
 public class TrianguloPascal {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Ingresa el nivel del triángulo de Pascal: ");
-        int nivel = s.nextInt();
+        int nivel;
+        do {
+            System.out.print("Ingresa el nivel del triángulo de Pascal: ");
+            nivel = s.nextInt();
+            pascal(nivel);
+        } while(nivel < 1);
         s.close();
-        pascal(nivel);
     }
     
     public static void pascal(int n) {
-        int[][] triangulo = new int[n + 1][n + 1];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
+        int i, j, k;
+        int[][] triangulo = new int[n][n];
+        
+        for (i = 0; i < n; i++) {
+            for (j = 0; j <= i; j++) {
                 if (j == 0 || j == i) {
                     triangulo[i][j] = 1;
                 } else {
                     triangulo[i][j] = triangulo[i - 1][j - 1] + triangulo[i - 1][j];
                 }
-                System.out.print(triangulo[i][j] + " ");
+            }
+        }
+        
+        int max = triangulo[n - 1][n / 2]; 
+        int ancho = String.valueOf(max).length() + 1;
+        
+        for (i = 0; i < n; i++) {
+            // espacios iniciales
+            for (k = 0; k < (n - i) * (ancho / 2); k++) {
+                System.out.print(" ");
+            }
+
+            for (j = 0; j <= i; j++) {
+                System.out.printf("%" + ancho + "d", triangulo[i][j]);
             }
             System.out.println();
         }
