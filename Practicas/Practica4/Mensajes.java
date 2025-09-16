a4ae884f5f40da0f3b88886ba323a4412100f891
amglezb <amglez.edu@gmail.com>
2025-09-16 12:04:40 -0600

    p4_v16_9

--- Practicas/Practica4/Mensajes.java
+++ Practicas/Practica4/Mensajes.java
@@ -2,6 +2,6 @@ import java.lang.Math;
 
 public class Mensajes {
     public String mensaje(double x0, double y0, double x1, double y1) {
-        return "La distancia entre  P1" + " (" + x0 + "," + y0 + ") " + " y " + "P2(" + x1 + "," + y1 + ")" + " es: " + Math.sqrt(Math.pow((x1 - x0), 2)+Math.pow((y1 - y0), 2));
+        return "La distancia entre  P0" + " (" + x0 + ", " + y0 + ") " + " y " + "P1 (" + x1 + ", " + y1 + ")" + " es " + Math.sqrt(Math.pow((x1 - x0), 2)+Math.pow((y1 - y0), 2)) + " unidades de distancia";
     }   
 }
