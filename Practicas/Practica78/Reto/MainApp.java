a961c1564ef74e1d150034b4ded72c5901f30d77
amglezb <amglez.edu@gmail.com>
2025-10-20 08:05:05 -0600

    presentado

--- Practicas/Practica78/Reto/MainApp.java
+++ Practicas/Practica78/Reto/MainApp.java
@@ -1,7 +1,6 @@
 public class MainApp {
     public static void main(String[] args) {
         Material[] misMateriales = new Material[3];
-
         misMateriales[0] = new Libro("Cien Años de Soledad", "Gabriel García Márquez", 1967, "Sudamericana");
         misMateriales[1] = new Revista("National Geographic", "Varios", 2023, 10);
         misMateriales[2] = new DVD("El Origen", "Christopher Nolan", 2010, 148);
