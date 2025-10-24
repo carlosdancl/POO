dcdc77e524e3fa29be5bc67733490c68a44d4acc
amglezb <amglez.edu@gmail.com>
2025-10-24 07:56:28 -0600

    main

---
+++ Proyectos/Proyecto2/MainApp.java
@@ -0,0 +1,29 @@
+package mx.unam.fi.poo.project2;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Empleado empAsalariado = new EmpleadoAsalariado("Daniel", "Contreras", "111-111-111", 12000.50);
+        Empleado empComision = new EmpleadoPorComision("Augusto", "Hori", "222-222-222", 8000.00, 0.15);
+        Empleado[] empleados = new Empleado[2];
+        empleados[0] = empAsalariado
+        empleados[1] = empComision
+        System.out.println("----------------");
+        for(Empleado empleadoi : empleados){
+            System.out.println(empleadoi);
+            System.out.println("Ingresos: $%,.2f\n", empleadoi.ingresos());
+            System.out.println("----------------");
+        }
+        System.out.println("\n--- (ERRORES) ---");
+        try {
+            new EmpleadoAsalariado("Carlos", "Ruiz", "333", -500.00);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error al crear empleado: " + e.getMessage());
+        }
+
+        try {
+            new EmpleadoPorComision("Maria", "Gómez", "444", 10000, 1.5); // Tarifa > 1.0
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error al crear empleado: " + e.getMessage());
+        }
+    }
+}
