2169c78920f00deb724bcd033a20805813c948b1
carlosdancl <contreras.l.carlos.daniel@gmail.com>
2025-10-24 07:48:26 -0600

    Class Empleado

---
+++ Proyectos/Proyecto2/Empleado.java
@@ -0,0 +1,33 @@
+package mx.unam.fi.poo.project2;
+
+public abstract class Empleado {
+    private final String nombre;
+    private final String apellidoPaterno;
+    private final String nss;
+
+    public Empleado(String nombre, String apellidoPaterno, String nss) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.nss = nss;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public String getNss() {
+        return nss;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Empleado: %s %s%nNúmero de Seguro Social: %s", nombre, apellidoPaterno, nss);
+    }
+
+    public abstract double ingresos();
+
+}
